7f7bab724c4bcf10abd43c48935ffcc933137cec
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-08-22 21:39:28 +0900

    최소사양 구현

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useCallback, useEffect, useState } from "react";
 
 const useDebounce = (func, wait) => {
@@ -14,22 +15,100 @@ const useDebounce = (func, wait) => {
     }, wait);
   };
 };
-
-const Todo = ({ content, onEdit, onClickDeleteBtn }) => {
+const TodoLi = styled.div`
+  input[type="text"] {
+    all: unset;
+  }
+  input[type="checkbox"] {
+    display: none;
+  }
+  input[type="checkbox"] + label {
+    height: 20px;
+    width: 20px;
+    background-color: transparent;
+    border: 2px solid #000;
+    border-radius: 50%;
+    box-sizing: border-box;
+    position: relative;
+    display: inline-block;
+    transition: border-color ease 0.2s;
+    cursor: pointer;
+  }
+  
+  input[type="checkbox"] + label::before,
+  input[type="checkbox"] + label::after {
+    position: absolute;
+    height: 0;
+    width: 4px;
+    background-color: #34b93d;
+    box-sizing: border-box;
+    display: inline-block;
+    transform-origin: left top;
+    border-radius: 5px;
+    content: " ";
+    transition: opacity ease 0.5;
+  }
+  input[type="checkbox"] + label::before,
+  input[type="checkbox"] + label::after {
+    background-color: #4285f4;
+  }
+  input[type="checkbox"] + label::before {
+    top: 14.4px;
+    left: 8.2px;
+    box-shadow: 0 0 0 1px transparent;
+    transform: rotate(-135deg);
+  }
+  input[type="checkbox"] + label::after {
+    top: 7.4px;
+    left: 1px;
+    transform: rotate(-45deg);
+  }
+  input[type="checkbox"]:checked + label {
+    border-color: #4285f4;
+  }
+  input[type="checkbox"]:checked + label::after {
+    height: 10px;
+    animation: bottomcheck 0.2s ease 0s forwards;
+  }
+  input[type="checkbox"]:checked + label::before {
+    height: 24px;
+    animation: topcheck 0.4s ease 0s forwards;
+  }
+  @keyframes bottomcheck {
+    0% {
+      height: 0;
+    }
+    100% {
+      height: 10px;
+    }
+  }
+  @keyframes topcheck {
+    0% {
+      height: 0;
+    }
+    50% {
+      height: 0;
+    }
+    100% {
+      height: 24px;
+    }
+  }
+  `;
+const Todo = ({ id, content, onEdit, onClickDeleteBtn }) => {
   const [text, setText] = useState(content);
   const [isEdit, setIsEdit] = useState(onEdit);
   const [isChecked, setIsChecked] = useState(false);
 
   const deleteTodo = useCallback(useDebounce(onClickDeleteBtn, 1000), []);
   const onBlur = () => {
-    if (!text) onClickDeleteBtn()
-    setIsEdit(false)
-  }
-
+    if (!text) onClickDeleteBtn();
+    setIsEdit(false);
+  };
 
   return (
-    <li>
+    <TodoLi>
       <input
+        id={id}
         type="checkbox"
         checked={isChecked}
         onChange={() => {
@@ -37,9 +116,11 @@ const Todo = ({ content, onEdit, onClickDeleteBtn }) => {
           deleteTodo(isChecked);
         }}
       />
+      <label htmlFor={id} className="cb2"></label>
       {isEdit ? (
         <>
           <input
+            className="edit-input"
             value={text}
             onChange={(e) => setText(e.target.value)}
             type="text"
@@ -49,11 +130,11 @@ const Todo = ({ content, onEdit, onClickDeleteBtn }) => {
           <button>i</button>
         </>
       ) : (
-        <>
-          <span onClick={() => setIsEdit(true)}>{text}</span>
-        </>
+        <span onClick={() => setIsEdit(true)}>{text}</span>
       )}
-    </li>
+
+      <hr></hr>
+    </TodoLi>
   );
 };
 
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,48 +1,59 @@
-import { nanoid } from "nanoid";
+import styled from "styled-components";
 import { useEffect, useRef, useState } from "react";
 import Todo from "../components/Todo";
+import { nanoid } from "nanoid";
 
 const sampleTodos = [
-  { id: 0, content: "content1", label: "label1", completed: false},
-  { id: 1, content: "content2", label: "label2", completed: false},
-  { id: 2, content: "content3", label: "label1", completed: false},
+  { id: 0, content: "content1", label: "label1", completed: false },
+  { id: 1, content: "content2", label: "label2", completed: false },
+  { id: 2, content: "content3", label: "label1", completed: false },
 ];
 
+const TodoListContainer = styled.div`
+  h1 {
+    font-size: 3rem;
+  }
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+`;
+
 const TodoList = () => {
-  const [todos, setTodos] = useState(sampleTodos)
-  const [hasNewTodo, setHasNewTodo] = useState(false)
-  const UlRef = useRef()
-  useEffect(()=> {
-    setHasNewTodo(false)
-  }, [hasNewTodo])
+  const [todos, setTodos] = useState(sampleTodos);
+  const [hasNewTodo, setHasNewTodo] = useState(false);
+  const UlRef = useRef();
+  useEffect(() => {
+    setHasNewTodo(false);
+  }, [hasNewTodo]);
   const handleDelete = (DeleteId) => {
-    setTodos(prev => prev.filter(({id})=> id!==DeleteId))
-  }
+    setTodos((prev) => prev.filter(({ id }) => id !== DeleteId));
+  };
   const addTodo = () => {
-    setTodos(prev => [...prev, 
-      { id: nanoid(), content: "", label: "label1", completed: false}
-    ])
-    setHasNewTodo(true)
-  }
+    setTodos((prev) => [
+      ...prev,
+      { id: nanoid(), content: "", label: "label1", completed: false },
+    ]);
+    setHasNewTodo(true);
+  };
   return (
-    <>
+    <TodoListContainer>
       <h1>할일</h1>
       <ul ref={UlRef}>
         {todos
-        .filter(({completed}) => !completed)
-        .map(({ id, content }) => (
-          <Todo
-            key={id}
-            content={content}
-            onClickDeleteBtn={()=>handleDelete(id)}
-            onEdit={hasNewTodo}
-          ></Todo>
-        ))}
+          .filter(({ completed }) => !completed)
+          .map(({ id, content }) => (
+            <Todo
+              key={id}
+              id={id}
+              content={content}
+              onClickDeleteBtn={() => handleDelete(id)}
+              onEdit={hasNewTodo}
+            ></Todo>
+          ))}
       </ul>
-      <button
-      onClick={addTodo}
-      >new todo</button>
-    </>
+      <button onClick={addTodo}>new todo</button>
+    </TodoListContainer>
   );
 };
 
